d8fe2b2c550b69f2af739ff8c381152cf96656bc
Clecius J. Martinkoski <clecius@e-precise.com.br>
2017-03-20 10:59:37 -0300

    modified:   main.c 	new file:   teste.bmp

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "stenography.h"
+#define MAX_NAME 256 /* tamanho maximo para nome de arquivo */
+#define MAX 1020 /* tamanho maximo para as matrizes e, consequentemente, para a figura, em pixels)*/
 /**
  * Lê um arquivo P3 como uma string
  */
@@ -22,8 +25,38 @@ int main() {
 }
 
 char *readP3File(char* path) {
+	FILE *arq1;
+	char key[128];
+	int i, j, n, m, max;
+	int r, g, b;
 
+	arq1 = fopen(path, "r");
+	if (arq1 == NULL) {
+		printf("Erro na abertura do arquivo %s\n", path);
+		return 0;
+	}
+
+	fscanf(arq1, "%s", key);
+	if (strcmp(key, "P3") != 0) {
+		printf("Arquivo nao e um PPM\n");
+		fclose(arq1);
+		return 0;
+	}
+
+	fscanf(arq1, "%d %d %d", &m, &n, &max);
+	for (i = 0; i <= n - 1; i++)
+		for (j = 0; j <= m - 1; j++) {
+
+			fscanf(arq1, " %d %d %d ", &r, &g, &b); /* para analise de caracteres trocar printf pela funcao que ira analisar a mensagem*/
+
+			printf(" %d %c %d \n ", r, g, b);
+
+		}
+	fclose(arq1);
+
+	return 0;
 }
+
 void writeP3File(char* path, char* content) {
 
 }
